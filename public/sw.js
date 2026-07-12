ae62736725a0662c86c46ee44245e2e1f0a7cc78
Eliasflpd <mc1armelo@gmail.com>
2026-07-12 13:04:14 -0300

    Bump SW cache to radar-v7 to force index.html refresh

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const V='radar-v6';
+const V='radar-v7';
 const CACHE=['/','/manifest.json','/capa.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(CACHE)));self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
