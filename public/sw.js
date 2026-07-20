d4e88433325c2978943585a4e00c40fde50422e3
Eliasflpd <mc1armelo@gmail.com>
2026-07-19 23:42:54 -0300

    RADAR: add video Leitura Biblica em Classe no inicio do carrossel (comprimido 26->6MB); SW v38

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const V='radar-v37';
+const V='radar-v38';
 const CACHE=['/','/manifest.json','/capa.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(CACHE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
